b266f6f825ac24e81c19a20d523b6ad709e55516
duaneywadey <idrdequito.cvt@eac.edu.ph>
2024-11-24 20:01:25 +0800

    add sample file

--- 1123lecforsoftenglec/1_uploading_deleting_files_in_php/handleForms.php
+++ 1123lecforsoftenglec/1_uploading_deleting_files_in_php/handleForms.php
@@ -1,32 +1,32 @@
 <?php  
 session_start();
-// if (isset($_POST['insertTextFileBtn'])) {
+if (isset($_POST['insertTextFileBtn'])) {
 
-// 	// Get file name
-// 	$fileName = $_FILES['textFile']['name'];
+	// Get file name
+	$fileName = $_FILES['textFile']['name'];
 
-// 	// Get temporary file name
-// 	$tempFileName = $_FILES['textFile']['tmp_name'];
+	// Get temporary file name
+	$tempFileName = $_FILES['textFile']['tmp_name'];
 
-// 	// Get file extension
-// 	$fileExtension = pathinfo($fileName, PATHINFO_EXTENSION);
+	// Get file extension
+	$fileExtension = pathinfo($fileName, PATHINFO_EXTENSION);
 
-// 	// Generate random characters for image name
-// 	$uniqueID = sha1(md5(rand(1,9999999)));
+	// Generate random characters for image name
+	$uniqueID = sha1(md5(rand(1,9999999)));
 
-// 	// Combine image name and file extension
-// 	$imageName = $uniqueID.".".$fileExtension;
+	// Combine image name and file extension
+	$imageName = $uniqueID.".".$fileExtension;
 
-// 	// Specify path
-// 	$folder = "files/".$imageName;
+	// Specify path
+	$folder = "files/".$imageName;
 
-// 	// Move file to the specified path 
-// 	if (move_uploaded_file($tempFileName, $folder)) {
-// 		$_SESSION['message'] = "File saved successfully!";
-// 		header("Location: index.php");
-// 	}
+	// Move file to the specified path 
+	if (move_uploaded_file($tempFileName, $folder)) {
+		$_SESSION['message'] = "File saved successfully!";
+		header("Location: index.php");
+	}
 	
-// }
+}
 
 
 // // Check if the form has been submitted by checking if the 'insertTextFileBtn' is set in the POST request

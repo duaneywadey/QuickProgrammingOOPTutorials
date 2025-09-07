98cce506febaebf60df745ee5e8a7dac85c05724
duaneywadey <idrdequito.cvt@eac.edu.ph>
2025-09-07 23:10:06 +0800

    add new notes, fiverr clone

---
+++ 2526_0906_lecforsoftenglec/2_fiverr_clone/client/core/handleForms.php
@@ -0,0 +1,82 @@
+<?php  
+require_once '../classloader.php';
+
+if (isset($_POST['insertNewUserBtn'])) {
+	$username = htmlspecialchars(trim($_POST['username']));
+	$email = htmlspecialchars(trim($_POST['email']));
+	$password = trim($_POST['password']);
+	$confirm_password = trim($_POST['confirm_password']);
+
+	if (!empty($username) && !empty($email) && !empty($password) && !empty($confirm_password)) {
+
+		if ($password == $confirm_password) {
+
+			if (!$userObj->usernameExists($username)) {
+
+				if ($userObj->registerUser($username, $email, $password)) {
+					header("Location: ../login.php");
+				}
+
+				else {
+					$_SESSION['message'] = "An error occured with the query!";
+					$_SESSION['status'] = '400';
+					header("Location: ../register.php");
+				}
+			}
+
+			else {
+				$_SESSION['message'] = $username . " as username is already taken";
+				$_SESSION['status'] = '400';
+				header("Location: ../register.php");
+			}
+		}
+		else {
+			$_SESSION['message'] = "Please make sure both passwords are equal";
+			$_SESSION['status'] = '400';
+			header("Location: ../register.php");
+		}
+	}
+	else {
+		$_SESSION['message'] = "Please make sure there are no empty input fields";
+		$_SESSION['status'] = '400';
+		header("Location: ../register.php");
+	}
+}
+
+if (isset($_POST['loginUserBtn'])) {
+	$email = trim($_POST['email']);
+	$password = trim($_POST['password']);
+
+	if (!empty($email) && !empty($password)) {
+
+		if ($userObj->loginUser($email, $password)) {
+			header("Location: ../index.php");
+		}
+		else {
+			$_SESSION['message'] = "Username/password invalid";
+			$_SESSION['status'] = "400";
+			header("Location: ../login.php");
+		}
+	}
+
+	else {
+		$_SESSION['message'] = "Please make sure there are no empty input fields";
+		$_SESSION['status'] = '400';
+		header("Location: ../login.php");
+	}
+
+}
+
+if (isset($_GET['logoutUserBtn'])) {
+	$userObj->logout();
+	header("Location: ../index.php");
+}
+
+if (isset($_POST['updateUserBtn'])) {
+	$contact_number = htmlspecialchars($_POST['contact_number']);
+	$bio_description = htmlspecialchars($_POST['bio_description']);
+	if ($userObj->updateUser($contact_number, $bio_description, $_SESSION['user_id'])) {
+		header("Location: ../profile.php");
+	}
+}
+
---
+++ 2526_0906_lecforsoftenglec/2_fiverr_clone/client/project_offers_submitted.php
@@ -0,0 +1,47 @@
+<?php require_once 'classloader.php'; ?>
+<!doctype html>
+  <html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+    <style>
+      body {
+        font-family: "Arial";
+      }
+    </style>
+  </head>
+  <body>
+    <?php include 'includes/navbar.php'; ?>
+    <div class="container-fluid">
+      <div class="display-4 text-center">Hello there and welcome! </div>
+      <div class="row justify-content-center">
+        <div class="col-md-12">
+          <div class="card shadow mt-4 mb-4">
+            <div class="card-body">
+              <div class="row">
+                <div class="col-md-6">
+                  <h2><a href="#">Ivan</a></h2>
+                  <img src="https://images.unsplash.com/photo-1755371034010-51c25321312d?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="img-fluid" alt="">
+                  <p class="mt-4 mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ab, eum alias quaerat minus! Delectus tenetur, maxime velit, deleniti eius sint vero. Atque tempora nam natus magni architecto voluptas, distinctio!</p>
+                  <h4><i>5000 - 10000 PHP</i></h4>
+                  <div class="float-right">
+                    <a href="#">Check out services</a>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <h2><a href="#">All Offers</a></h2>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing, elit. Illo tempora atque voluptates delectus quos, expedita velit, magnam vitae ut error eligendi sint provident iure esse dolor dolorem alias cumque earum?</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
